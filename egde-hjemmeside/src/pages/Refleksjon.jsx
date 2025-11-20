dd87b1dbc3e3722bccf638c863da9edf01236fda
Simon Portillo <simon.portillo@outlook.com>
2025-11-20 18:52:16 +0100

    avsluttende møte seksjon

--- egde-hjemmeside/src/pages/Refleksjon.jsx
+++ egde-hjemmeside/src/pages/Refleksjon.jsx
@@ -44,8 +44,28 @@ function Refleksjon() {
                         Praksisen har også gitt oss et større innblikk i arbeidslivet. Vi har fått se hvordan teori 
                         omvendes til praktisk arbeid, og hvordan ulike teknologier brukes i reelle prosjekter. Denne 
                         erfaringen har gjort oss mer forberedt og bedre rustet til videre arbeid og fremtidige jobber 
-                        innen feltet.
+                        innen feltet.   
                     </p>
+                    <h2 className="section-title">Hva er det viktigste vi har lært</h2>
+                        <ul>
+                            <li>Next.js, React med TypeScript og TailwindCSS</li>
+                            <li>Hvordan oversette design fra Figma til gjenbrukbare komponenter</li>
+                            <li>Hvordan det er å jobbe selvstendig i et team</li>
+                        </ul>
+                    <h2 className="section-title">Mest utfordrende</h2>
+                        <ul>
+                            <li>Å sette seg inn i nye teknologier på kort tid</li>
+                            <li>Tolke design fra Figma / manglende dokumentasjon</li>
+                            <li>Balansere hvor mye en komponent skal ha ansvar for</li>
+                        </ul>
+                    <h2 className="section-title">Det rareste/gøyeste fra praksisperioden</h2>
+                        <ul>
+                            <li>Sosiale aktiviteter</li>
+                            <ul>
+                                <li>Spillkvelder</li>
+                                <li>Oktoberfest</li>
+                            </ul> 
+                        </ul>
                 </div>
             </section>
         </div>
